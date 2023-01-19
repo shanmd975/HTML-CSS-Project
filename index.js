91f90990cc0914c321ac4a9147a4149a5656da1b
Mohammad Shanoor <107303734+shanmd975@users.noreply.github.com>
2023-01-20 01:23:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-  
-  const header = document.createElement("header");
-  header.innerHTML = '<h1>Mobile Navigation</h1>';
-  document.body.prepend(header);
-
-  const footer = document.createElement("footer");
-  footer.innerHTML = '<h2>Created By Mohammad Shan</h2>';
-  document.body.appendChild(footer);
-
-  const contents = document.querySelectorAll(".content");
-  const listItems = document.querySelectorAll("nav ul li");
-
-  const hideAllContents = () => {
-    contents.forEach((content) => content.classList.remove("show"));
-  };
-
-  const hideAllItems = () => {
+const contents = document.querySelectorAll(".content");
+const listItems = document.querySelectorAll("nav ul li");
+const hideAllContents = () => {
+  contents.forEach((content) => content.classList.remove("show"));
+};
+const hideAllItems = () => {
     listItems.forEach((item) => item.classList.remove("active"));
-  };
-  listItems.forEach((item, index) => {
-    item.addEventListener("click", () => {
+};
+listItems.forEach((item, index) => {
+  item.addEventListener("click",() => {
       hideAllContents();
       hideAllItems();
       item.classList.add("active");
       contents[index].classList.add("show");
-    });
   });
 });
-
-
